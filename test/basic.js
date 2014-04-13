60ace12219b5c7fd5d4a190f62af85711368f3af
Bret Copeland <bret@atlantisflight.org>
2014-04-13 04:01:00 -0400

    Restarting development on this project...

--- test/basic.js
+++ test/basic.js
@@ -1,14 +1,30 @@
 "use strict";
 
-var assert = require('chai').assert;
+var assert = require('assert');
 var config = require('./config.js');
-var Neorm = require('../lib/neorm.js');
+var Graph = require('../lib').Graph;
 
-describe('Connect', function ()
+suite('Basic Functions', function ()
 {
-	it('should ping the neo4j server', function (done)
+	test('Ping the neo4j server', function * ()
 	{
-		var n = new Neorm(config.server);
-		n.ping(done);
+		let graph = new Graph(config.server);
+		assert(yield graph.ping());
+	});
+	
+	test('Get the root node', function * ()
+	{
+		let graph = new Graph(config.server);
+		let res = yield graph.request('get', '/node/0');
+		
+		assert(res.statusCode === 200);
+		assert(res.body);
+		assert(/node\/0$/.test(res.body.self));
+	});
+	
+	test('Get root via Cypher', function * ()
+	{
+		let graph = new Graph(config.server);
+		let res = yield graph.query('START n = node(0) RETURN n;');
 	});
 });
